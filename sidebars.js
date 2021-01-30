eeadd3b38354a335e82b80468d3bce6f36b75179
edfeldman11 <contact@edfeldman.co.uk>
2021-01-30 14:32:10 +0000

    Filling out the docs

--- sidebars.js
+++ sidebars.js
@@ -1,15 +1,30 @@
 module.exports = {
   someSidebar: {
-    "Overview": ['purpose', 'usage', 'history'],
-    "How do I start my own dashboard?": ['requesting-dashboard'],
+    "Overview": [
+      'purpose',
+      'usage',
+      'history'
+    ],
+    "How do I start my own dashboard?": [
+      'types-of-dashboard',
+      'hosting',
+      'requesting-dashboard'
+    ],
     "Setting up your dashboard": [
       'tracker-planning',
       'dashboard-planning',
       'dashboard-roles',
       'hints-and-tips'
     ],
-    "Using your dashboard": [],
-    "Data and Security": [],
-    "Frequently asked questions": []
+    "Using your dashboard": [
+      'using-the-action-tracker',
+      'using-the-full-dashboard'
+    ],
+    "Data and Security": [
+      'data-security'
+    ],
+    "Frequently asked questions": [
+      'frequently-asked-questions'
+    ]
   },
 };
